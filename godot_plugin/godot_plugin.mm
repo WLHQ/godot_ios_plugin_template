8c433a6637939d8b21799772d1a01cd594d7e391
WLHQ <106452668+WLHQ@users.noreply.github.com>
2025-01-10 11:02:20 -0500

    Update godot_plugin.mm

--- godot_plugin/godot_plugin.mm
+++ godot_plugin/godot_plugin.mm
@@ -10,22 +10,33 @@
 
 #import "godot_plugin.h"
 #import "godot_plugin_implementation.h"
+#import "godot_plugin_delegate.h"  // Import your custom iOS delegate
 
 #import "core/config/engine.h"
 
 PluginExample *plugin;
+GodotPluginDelegate *pluginDelegate;
 
 void godot_plugin_init() {
     NSLog(@"init plugin");
 
+    // Create and register the PluginExample instance
     plugin = memnew(PluginExample);
     Engine::get_singleton()->add_singleton(Engine::Singleton("PluginExample", plugin));
+
+    // Initialize the iOS delegate
+    pluginDelegate = [[GodotPluginDelegate alloc] init];
+    [[UIApplication sharedApplication] setDelegate:pluginDelegate];
 }
 
 void godot_plugin_deinit() {
     NSLog(@"deinit plugin");
-    
+
+    // Free the PluginExample instance
     if (plugin) {
-       memdelete(plugin);
-   }
+        memdelete(plugin);
+    }
+
+    // Clean up the delegate
+    pluginDelegate = nil;
 }
